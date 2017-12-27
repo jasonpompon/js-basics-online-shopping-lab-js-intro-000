6f4ab161015c7c9c68fa4ab6050cc322e70268b3
Jason Pompon <jasonpompon@gmail.com>
2017-12-27 03:01:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ function removeFromCart(item) {
           return cart
       } else {
           console.log('That item is not in your cart.');
-          return cart
+        //   return cart
       }
 
   }
