1d8f4c9e6a576ad0ec685708c9a4b14ee10a027f
Jason Pompon <jasonpompon@gmail.com>
2017-12-27 00:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,18 +11,70 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
+ const randomPrice = Math.floor(Math.random() * 100)
+
+ // create object from item name and price
+ const itemObj = { [item] : randomPrice }
+
+ // add newly created object to cart array
+ if (itemObj) {
+     cart.push(itemObj)
+     console.log(`${item} has been added to your cart.`)
+     return cart;
+ }
 }
 
 function viewCart() {
   // write your code here
+  // loop over array of objects and print key value pairs in a string
+
+  var statement;
+
+  if (cart.length === 0) {
+      statement = 'Your shopping cart is empty.';
+      console.log(statement);
+  } else if (cart.length === 1) {
+      statement = `In your cart, you have ${Object.keys(cart[0]).join()} at $${Object.values(cart[0]).join()}.`
+      console.log(statement);
+  } else if (cart.length === 2) {
+      statement = `In your cart, you have ${Object.keys(cart[0]).join()} at $${Object.values(cart[0]).join()} and ${Object.keys(cart[1]).join()} at $${Object.values(cart[1]).join()}.`
+      console.log(statement);
+  } else {
+      
+  }
+
+  return statement;
 }
 
 function total() {
   // write your code here
+  let totalCost = 0;
+  for (let i = 0; i < cart.length; i++) {
+      const itemName = Object.keys(cart[i]);
+      const itemPrice = cart[i][itemName];
+
+      totalCost = totalCost + itemPrice
+  }
+
+  return totalCost
 }
 
 function removeFromCart(item) {
   // write your code here
+  // for (let i = 0; i < cart.length; i++) {
+  // 
+  // 
+  // 
+  // 
+  // 
+  //     if (cart[i].hasOwnProperty(item)) {
+  //         cart = [...cart.slice(0,i),...cart.slice(i+1)]
+  //     } else if (cart[i].hasOwnProperty(item) === false) {
+  //         console.log('That item is not in your cart.');
+  //     }
+  // }
+  // 
+  // return cart
 }
 
 function placeOrder(cardNumber) {
