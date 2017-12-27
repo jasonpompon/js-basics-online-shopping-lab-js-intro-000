49eb1832e47de20db940564fceab5f395717aff1
Jason Pompon <jasonpompon@gmail.com>
2017-12-27 02:58:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,13 +82,12 @@ function removeFromCart(item) {
 
       if (cart[i].hasOwnProperty(item)) {
           cart = [...cart.slice(0,i),...cart.slice(i+1)]
+          return cart
       } else if (cart[i].hasOwnProperty(item) === false) {
           console.log('That item is not in your cart.');
       }
 
   }
-
-  return cart
 }
 
 function placeOrder(cardNumber) {
