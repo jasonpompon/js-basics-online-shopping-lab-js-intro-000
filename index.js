a16ed019e352319907430a9d62fe8cac2c90f980
Jason Pompon <jasonpompon@gmail.com>
2017-12-27 08:35:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -80,13 +80,16 @@ function removeFromCart(item) {
   // write your code here
   for (let i = 0; i < cart.length; i++) {
 
-      if (cart[i].hasOwnProperty(item)) {
-          cart = [...cart.slice(0,i),...cart.slice(i+1)]
-
-      } else {
+      if (cart[i].hasOwnProperty(item) === false) {
           console.log('That item is not in your cart.');
-        //   return cart
       }
+
+    //   if (cart[i].hasOwnProperty(item)) {
+    //       cart = [...cart.slice(0,i),...cart.slice(i+1)]
+      // 
+    //   } else {
+    //       console.log('That item is not in your cart.');
+    //   }
 
   }
 
